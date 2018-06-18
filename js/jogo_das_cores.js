69b2c2ebeba110faacec18d0d874063ec06efe67
unknown <ozzy23123@gmail.com>
2018-06-18 12:29:30 -0300

    testes

--- js/jogo_das_cores.js
+++ js/jogo_das_cores.js
@@ -164,41 +164,46 @@ window.onload = function() {
                 // Clusters need to be found and removed
                 if (animationtime > animationtimetotal) {
                     // Find clusters
-                    findClusters();
-                    
-                    if (clusters.length > 0) {
+                    findClusters();                    
+                        switch(cor){
+                            case 0:
+                                $("#viewport").hide();
+                                $("#animation6").show();
+                                aaa = 9;
+                                anima(9);
+                                sleep(700);
+                                break;
+                            case 1:
+                                $("#viewport").hide();
+                                $("#animation7").show();
+                                aaa = 10;
+                                anima(10);
+                                sleep(700);
+                                break;
+                            case 2:
+                                $("#viewport").hide();
+                                $("#animation8").show();
+                                aaa = 11;
+                                anima(11);
+                                sleep(700);
+                                break;
+                            case 3:
+                                $("#viewport").hide();
+                                $("#animation9").show();
+                                aaa = 12;
+                                anima(12);
+                                sleep(700);
+                                break;
+                        }
+                        if (clusters.length > 0) {
                         // Add points to the score
-                        for (var i=0; i<clusters.length; i++) {
+                        for (var i=0; i<1; i++) {
                             // Add extra points for longer clusters
                             if (score < 2000) {
-                                score += 100 * (clusters[i].length - 2);
-                                if (cor == 0) {
-                                    // alert("Amarelo!");
-                                    $("#viewport").hide();
-                                    $("#animation6").show();
-                                    anima(9);
-                                    aaa = 9;
-                                }else if(cor == 1){
-                                    // alert("Verde!");
-                                    $("#viewport").hide();
-                                    $("#animation7").show();
-                                    anima(10);
-                                    aaa = 10;
-                                }else if(cor == 2){
-                                    // alert("Azul!");
-                                    $("#viewport").hide();
-                                    $("#animation8").show();
-                                    anima(11);
-                                    aaa = 11;
-                                }else if(cor == 3){
-                                    // alert("Vermelho!");
-                                    $("#viewport").hide();
-                                    $("#animation9").show();
-                                    anima(12);
-                                    aaa = 12;   
+                                score += 200;
                                 }
                             }
-                        }
+                        
                         if(score == 2000){
                             $("#score_cc_text").html("Você é top!");
                         }
